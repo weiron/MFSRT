979bfe5e7be2ba97444a60321868f20019af8172
Keisuke Matsumura <zokweiron@weiron-iMac.local>
2015-03-19 19:38:39 +0900

    Initial Commit

---
+++ MFSRT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MFSRT
+//
+//  Created by Keisuke Matsumura on 2015/03/19.
+//  Copyright (c) 2015年 Keisuke Matsumura. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
